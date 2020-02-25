6a73ba36f855c62a62f8895a409b2b08cacad7dc
peetck <peekung602@gmail.com>
2020-02-25 20:57:46 +0700

    Update
    - edit
    - vote

--- kod-vote/polls/urls.py
+++ kod-vote/polls/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'), # index page
-    path('create/', views.create_view, name='create') # create poll page
+    path('create/', views.create_view, name='create'), # create poll page
+    path('vote/<int:poll_id>', views.vote_view, name='vote'),
+    path('edit/<int:poll_id>', views.edit_view, name='edit')
 ]
--- kod-vote/polls/views.py
+++ kod-vote/polls/views.py
@@ -6,3 +6,11 @@ def index_view(request):
 
 def create_view(request):
     return render(request, 'create.html')
+
+def vote_view(request, poll_id):
+    context = {'poll_id' : poll_id}
+    return render(request, 'vote.html', context)
+
+def edit_view(request, poll_id):
+    context = {'poll_id' : poll_id}
+    return render(request, 'edit.html', context)
