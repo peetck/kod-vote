e6a5f35589a4fcf6e4d58bc54a3c882d41ae3203
peetck <peekung602@gmail.com>
2020-03-02 23:09:51 +0700

    Update default datetime picker

--- kod-vote/static/js/script.js
+++ kod-vote/static/js/script.js
@@ -1,7 +1,8 @@
 $(function () {
     $('#datetimepicker1').datetimepicker({
         locale: 'th',
-        minDate : new Date()
+        minDate : new Date(),
+        defaultDate: new Date()
     });
     $('#datetimepicker2').datetimepicker({
         locale: 'th',
