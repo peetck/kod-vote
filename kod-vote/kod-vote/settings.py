ce4f45dc026aac397611dde937ae82c9eed92870
peetck <peekung602@gmail.com>
2020-03-08 16:22:56 +0700

    Update
    - make media img automatically delete
    - install django-cleanup

--- kod-vote/kod-vote/settings.py
+++ kod-vote/kod-vote/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # my own app
     'polls',
-    'accounts'
+    'accounts',
+    # django-cleanup
+    'django_cleanup'
 ]
 
 MIDDLEWARE = [
